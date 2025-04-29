a50e33eb8aabb34899716bb1b80dffe6204406ca
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-04-29 22:01:09 +0200

    Update CasterProvider.php

--- src/CasterProvider.php
+++ src/CasterProvider.php
@@ -7,59 +7,40 @@ use Bermuda\Stdlib\Arrayable;
 /**
  * Class CasterProvider
  *
- * A provider responsible for managing and supplying caster instances.
- * Casters are used to convert values into specific types or formats.
+ * Responsible for managing and supplying caster instances.
+ * Casters convert values to specific types or formats.
+ * The provider maintains a registry, using caster names as keys.
  *
- * This provider maintains a registry of casters, where each caster is identified by its name.
- * Casters can be registered either as a class string or as an instance of CasterInterface.
- * When a caster is requested via the provide() method, if it's stored as a class string,
- * it will be instantiated on demand.
+ * Casters may be registered either as a class name (to be instantiated on demand)
+ * or as direct instances of CasterInterface.
  */
 final class CasterProvider implements \IteratorAggregate, Arrayable
 {
     /**
-     * An array of caster definitions indexed by the caster's name.
-     *
-     * Casters are stored either as class string names or as instantiated objects.
+     * An associative array of caster definitions.
+     * Keys are caster names, and values are either class strings or instantiated objects.
      *
      * @var CasterInterface[]
      */
-    private array $casters = [
-        'json' => JsonCaster::class,
-        'array' => ArrayCaster::class,
-        'base64' => Base64Caster::class,
-        'string' => StringCaster::class,
-        'int' => IntCaster::class,
-        'float' => FloatCaster::class,
-        'bool' => BooleanCaster::class,
-        'email' => EmailCaster::class,
-        'timestamp' => TimestampCaster::class,
-        'carbon' => CarbonCaster::class,
-        'uuid' => UuidCaster::class,
-        'slug' => SlugCaster::class,
-        'phone' => PhoneCaster::class,
-        'now' => NowCaster::class,
-        'byte' => ByteCaster::class,
-    ];
+    private array $casters = [];
 
     /**
      * Constructor.
      *
-     * Registers additional caster instances provided in the iterable.
+     * Optionally initializes the provider with an iterable collection of caster instances.
      *
      * @param iterable<CasterInterface> $casters An iterable collection of caster instances.
      */
     public function __construct(iterable $casters = [])
     {
-        foreach ($casters as $caster) {
-            $this->add($caster);
-        }
+        foreach ($casters as $caster) $this->add($caster);
     }
 
     /**
      * Adds a caster instance to the registry.
      *
-     * The caster is registered using the name returned by its getName() method.
+     * The caster instance is registered using its unique name
+     * as determined by the caster's getName() method.
      *
      * @param CasterInterface $caster The caster instance to add.
      * @return void
@@ -70,20 +51,22 @@ final class CasterProvider implements \IteratorAggregate, Arrayable
     }
 
     /**
-     * Returns an array representation of the registered casters.
+     * Returns an associative array representation of the registered casters.
      *
-     * @return array The array of caster definitions indexed by caster name.
+     * This method is part of the Arrayable interface implementation.
+     *
+     * @return array The array representation of casters, indexed by the caster name.
      */
     public function toArray(): array
     {
-        return array_map($this->casters);
+        return $this->casters;
     }
 
     /**
-     * Checks if a caster with the given name is registered.
+     * Determines if a caster with the specified name is registered.
      *
-     * @param string $name The name of the caster.
-     * @return bool True if the caster is registered; false otherwise.
+     * @param string $name The caster name to look for.
+     * @return bool Returns true if the caster exists; false otherwise.
      */
     public function has(string $name): bool
     {
@@ -91,56 +74,74 @@ final class CasterProvider implements \IteratorAggregate, Arrayable
     }
 
     /**
-     * Provides a caster instance by its name.
+     * Provides a caster instance based on its name.
      *
-     * If the caster is stored as a class string and the name contains a pipe ("|"),
-     * then multiple caster names are assumed to be provided and a PipeCaster is created to combine them.
-     * If the caster is stored as a class string, it is instantiated on demand.
+     * If the given name is composite (contains a pipe "|" character), it is assumed that
+     * multiple caster names have been provided. In this case, a composite PipeCaster is
+     * created that combines the individual casters.
      *
-     * @param string $name The name (or composite name) of the caster.
-     * @return CasterInterface|null The caster instance, or null if no caster is registered under the provided name.
+     * For caster definitions stored as class strings, they are instantiated on demand.
      *
-     * @throws \OutOfBoundsException If one of the composite casters is not found.
+     * @param string $name The name or composite name of the caster.
+     * @return CasterInterface|null The caster instance if found; otherwise, null.
+     *
+     * @throws \OutOfBoundsException If a composite caster references a caster that is not registered.
      */
     public function provide(string $name): ?CasterInterface
     {
         if (!isset($this->casters[$name]) && str_contains($name, '|')) {
             $casterNames = explode('|', $name);
-
+            
             $pipe = [];
             foreach ($casterNames as $casterName) {
                 if (!isset($this->casters[$casterName])) {
                     throw new \OutOfBoundsException("Caster '$casterName' not found for composite caster $name");
                 }
-
-                $instance = $this->casters[$casterName];
-                if (is_string($instance)) {
-                    $instance = new $instance();
-                    $this->casters[$casterName] = $instance;
-                }
-                $pipe[] = $instance;
+                
+                $pipe[] = $this->casters[$casterName];
             }
-
+            
             return $this->casters[$name] = new PipeCaster($pipe);
         }
-
-        $caster = $this->casters[$name] ?? null;
-        if (is_string($caster)) {
-            return $this->casters[$name] = new $caster();
-        }
-
-        return $caster;
+        
+        return $this->casters[$name] ?? null;
     }
 
     /**
      * Retrieves an iterator for the registered casters.
      *
-     * This method allows iteration over the casters using constructs such as foreach.
+     * This supports the use of iteration constructs (e.g., foreach) on the provider.
      *
      * @return \Generator<string, CasterInterface> A generator yielding caster name => caster instance pairs.
      */
     public function getIterator(): \Generator
     {
-        yield from $this->toArray();
+        yield from $this->casters;
+    }
+
+    /**
+     * Static method to create a default CasterProvider instance with a predefined set of casters.
+     *
+     * @return CasterProvider Returns a new provider preloaded with a set of common caster implementations.
+     */
+    public static function createDefault(): CasterProvider
+    {
+        return new CasterProvider([
+            new ArrayCaster,
+            new JsonCaster,
+            new Base64Caster,
+            new StringCaster,
+            new IntCaster,
+            new FloatCaster,
+            new BooleanCaster,
+            new EmailCaster,
+            new TimestampCaster,
+            new CarbonCaster,
+            new UuidCaster,
+            new SlugCaster,
+            new PhoneCaster,
+            new NowCaster,
+            new ByteCaster,
+        ]);
     }
 }
